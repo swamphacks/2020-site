3d35113d01f92b35f8be985dfd555ed590113bee
Zachary Cowan <cz8ch8ry@gmail.com>
2019-11-20 19:32:59 -0500

    Started on hamburger

--- src/components/ScrollNav.js
+++ src/components/ScrollNav.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
-
 import {Header, Responsive} from 'semantic-ui-react';
 import {scroller} from 'react-scroll';
+import {Spring} from 'react-spring/renderprops';
 
 const Container = styled.div`
   position: absolute;
@@ -10,6 +10,32 @@ const Container = styled.div`
   left: 0;
   z-index: 1000;
   padding: 40px;
+`;
+
+const Hamburger = styled.img`
+  fill: black;
+  width: 30px;
+  -webkit-transition: transform 0.2s;
+  -ms-transition: transform 0.2s;
+  transition: transform 0.2s;
+  :hover {
+    -webkit-transform: scale(1.1);
+    -ms-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+`;
+
+const Close = styled.img`
+  fill: black;
+  width: 30px;
+  -webkit-transition: transform 0.2s;
+  -ms-transition: transform 0.2s;
+  transition: transform 0.2s;
+  :hover {
+    -webkit-transform: scale(1.1);
+    -ms-transform: scale(1.1);
+    transform: scale(1.1);
+  }
 `;
 
 const List = styled.ul`
@@ -31,29 +57,86 @@ const Button = styled.div`
   }
 `;
 
+const Drawer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+`;
+
 const ScrollNav = ({items}) => {
+  const [open, setOpen] = useState(false);
   return (
-    <Responsive as={Container} minWidth={600}>
-      <List>
-        {items.map(({label, scrollId}, index) => {
-          return (
-            <Item key={label + index}>
-              <Button
-                onClick={() => {
-                  scroller.scrollTo(scrollId, {
-                    duration: 1000,
-                    delay: 50,
-                    smooth: true
-                  });
-                }}
-              >
-                <Header>{label}</Header>
-              </Button>
-            </Item>
-          );
-        })}
-      </List>
-    </Responsive>
+    <React.Fragment>
+      <Responsive as={Container} minWidth={600}>
+        <List>
+          {items.map(({label, scrollId}, index) => {
+            return (
+              <Item key={label + index}>
+                <Button
+                  onClick={() => {
+                    scroller.scrollTo(scrollId, {
+                      duration: 1000,
+                      delay: 50,
+                      smooth: true
+                    });
+                  }}
+                >
+                  <Header>{label}</Header>
+                </Button>
+              </Item>
+            );
+          })}
+        </List>
+      </Responsive>
+      <Responsive as={Container} maxWidth={600}>
+        <Hamburger
+          src='./images/hamburgerMenu.svg'
+          onClick={() => setOpen(true)}
+        />
+        {open && (
+          <Spring
+            from={{left: -100}}
+            to={{
+              left: 0
+            }}
+          >
+            {props => (
+              <Drawer style={props}>
+                <Close
+                  src='./images/hamburgerMenu.svg'
+                  onClick={() => setOpen(false)}
+                />
+                <List>
+                  {items.map(({label, scrollId}, index) => {
+                    return (
+                      <Item key={label + index} style={{float: 'none'}}>
+                        <Button
+                          onClick={() => {
+                            scroller.scrollTo(scrollId, {
+                              duration: 1000,
+                              delay: 50,
+                              smooth: true
+                            });
+                          }}
+                        >
+                          <Header>{label}</Header>
+                        </Button>
+                      </Item>
+                    );
+                  })}
+                </List>
+              </Drawer>
+            )}
+          </Spring>
+        )}
+      </Responsive>
+    </React.Fragment>
   );
 };
 
